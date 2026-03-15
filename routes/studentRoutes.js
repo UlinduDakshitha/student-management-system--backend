1980dd9beff4c29b65ad3115ab6222a0828cf252
Bandara W.M.U.D. <166277247+UlinduDakshitha@users.noreply.github.com>
2026-03-15 20:31:25 +0530

    update

---
+++ routes/studentRoutes.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const router = express.Router();
+const studentController = require("../controllers/studentController");
+const auth = require("../middleware/authMiddleware");
+
+router.post("/students", auth, studentController.createStudent);
+router.get("/students", auth, studentController.getStudents);
+router.get("/students/:id", auth, studentController.getStudent);
+router.put("/students/:id", auth, studentController.updateStudent);
+router.delete("/students/:id", auth, studentController.deleteStudent);
+
+module.exports = router;
