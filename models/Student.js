4602b48b09c82f709dd5562621eccba16490f337
Bandara W.M.U.D. <166277247+UlinduDakshitha@users.noreply.github.com>
2026-03-16 21:40:56 +0530

    update

--- models/Student.js
+++ models/Student.js
@@ -1,6 +1,7 @@
 const { DataTypes } = require("sequelize");
 const sequelize = require("../config/database");
 
+
 const Student = sequelize.define("Student", {
   name: DataTypes.STRING,
   email: DataTypes.STRING,
